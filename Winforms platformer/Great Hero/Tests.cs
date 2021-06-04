41c7a3211cc3e34c729414453ce675f88bd37190
NikitaSamkov <nick.samkov@yandex.ru>
2021-06-04 17:13:15 +0500

    added treasures into README.md

---
+++ Winforms platformer/Great Hero/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Winforms_platformer.Model;
+
+namespace Winforms_platformer
+{
+    [TestFixture]
+    public class TreasureTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void TestRoom()
+        {
+            Assert.AreEqual(Game.Player.CurrentRoom(), Game.Map.CurrentRoom());
+        }
+    }
+}
